206f87958a1bba3f162861699324213be2703e72
c0de13 <meganmpuppy@gmail.com>
2018-07-11 14:22:29 +0000

    automatically backed up by learn

--- amazon.rb
+++ amazon.rb
@@ -5,6 +5,8 @@ cart_item_prices << 5.50
 state_tax = 0.06 #this is the state tax in Florida
 
 
+
+count = 1 
 #for each item in the cart_item_prices
 cart_item_prices.each do |x|
 
@@ -12,6 +14,6 @@ cart_item_prices.each do |x|
   total_price = x * (1 + state_tax) 
 
 #output the price + tax, going to store in a new Array
-  puts total_price
+  puts "Item   #{total_price}."
 
 end
